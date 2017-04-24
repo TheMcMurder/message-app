46fa58c89f727685d55f13ec1ed3f585938dab76
Justin McMurdie <mcmurdiej@gmail.com>
2017-04-24 13:56:27 -0600

    Setting up some wercker stuff. I'm pretty sure this will work. I never remember exactly how to get wercker to run the scripts in my repo.

--- handler.js
+++ handler.js
@@ -1,16 +1,22 @@
 'use strict';
+var config = require('./config.ignored.js')
+var twilio = require('twilio')
 
 module.exports.sendMessage = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Serverless v1.0! Your function executed successfully!',
+  //     input: event,
+  //   }),
+  // };
 
-  callback(null, response);
+  // callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', config });
 };
+
+function sendMessage(message, toNumber) {
+  var twilioClient = twilio(config.twilioAccountSID, config.twilioAuthToken)
+}
